e25757b069eaa4ba969ea126e8b0d9acae057b2e
Chris Sell <baysideufl@gmail.com>
2013-11-18 00:59:04 -0500

    Fixed precomplie error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ RailsProject::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += *.png
+  config.assets.precompile += %w[*.png *.jpg *.jpeg *.git]
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
